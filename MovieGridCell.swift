42db2e97a209e214aeba4ec5ab5943b80d1af3a9
Monique <33104264+anamicieli@users.noreply.github.com>
2020-10-02 18:24:12 -0400

    added Flix

---
+++ MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Monique Carvalho on 9/30/20.
+//  Copyright © 2020 codepath. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
---
+++ ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flix
+//
+//  Created by Monique Carvalho on 9/23/20.
+//  Copyright © 2020 codepath. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
